bbee20b37ffe4283631c6d060977a098d07b65ad
diogo <matheusss.diogooo@gmail.com>
2026-09-22 21:51:09 -0300

    Update SimulaçãoMonteCarlo.R

--- SimulaçãoMonteCarlo.R
+++ SimulaçãoMonteCarlo.R
@@ -34,11 +34,12 @@ set.seed(2026)
 alpha <- 4
 beta <- 3
 
-### Determinar todos os tamanhos amostrais utilizados
+### Determinar todos os tamanhos amostrais utilizados e quantidade R de amostras para cada tamanho amostral
 tamanhos_amostrais <- c(20, 30, 50, 100, 200)
+R <- 10000
 
 ### Definir uma tabela das amostras com seus tamanhos amostrais e estimativas dos parâmetros
-amostras <- tibble(
+amostras_estimativas <- tibble(
     n = numeric(),
     est_alpha = numeric(),
     est_beta = numeric()
@@ -46,7 +47,7 @@ amostras <- tibble(
 
 ### Gerar 10000 amostras para cada tamanho amostral
 for (t in tamanhos_amostrais) {
-    for (r in 1:10000) {
+    for (r in 1:R) {
         #### Criar r-ésima amostra de tamanho amostral t
         amostra <- rgamma(n = t, shape = alpha, scale = beta)
 
@@ -54,7 +55,7 @@ for (t in tamanhos_amostrais) {
         tamanho_amostral <- t
 
         #### Estimar o parâmetro alpha pelo método numérico
-        intervalo_inicial <- c(1, 10)
+        intervalo_inicial <- c(1, 1)
         while (TRUE) {
             estimativa_alpha <- try(
                 estimar_alpha(amostra, intervalo = intervalo_inicial),
@@ -63,7 +64,7 @@ for (t in tamanhos_amostrais) {
             ifelse(
                 is.numeric(estimativa_alpha),
                 break,
-                intervalo_inicial <- intervalo_inicial * c(0.5, 2)
+                intervalo_inicial <- intervalo_inicial * c(1 / 2, 2)
             )
         }
 
@@ -71,7 +72,7 @@ for (t in tamanhos_amostrais) {
         estimativa_beta <- mean(amostra) / estimativa_alpha
 
         #### Adicionar a r-ésima amostra na tabela criada
-        amostras <- amostras %>%
+        amostras_estimativas <- amostras_estimativas %>%
             add_row(
                 n = tamanho_amostral,
                 est_alpha = estimativa_alpha,
@@ -82,38 +83,70 @@ for (t in tamanhos_amostrais) {
 
 ## Avaliação dos Estimadores #####################################################################################
 
-### Para amostras de 20 observações
+### Gerar a tabela com a avaliação dos estimadores para cada tamanho amostral
+for (t in tamanhos_amostrais) {
+    #### Calcular a média das estimativas
+    avaliacao_parcial_estimadores <- amostras_estimativas %>%
+        filter(n == t) %>%
+        summarise(
+            tamanho_amostral = t,
+            med_est_alpha = mean(est_alpha),
+            med_est_beta = mean(est_beta)
+        )
 
-#### Calcular a média das estimativas
-avaliacao_estimadores <- amostras %>%
-    filter(n == 200) %>%
-    summarise(med_est_alpha = mean(est_alpha), med_est_beta = mean(est_beta))
+    #### Calcular o viés
+    avaliacao_parcial_estimadores <- avaliacao_parcial_estimadores %>%
+        mutate(
+            vies_est_alpha = med_est_alpha - alpha,
+            vies_est_beta = med_est_beta - beta
+        )
 
-#### Calcular o viés
-avaliacao_estimadores <- avaliacao_estimadores %>%
-    mutate(
-        vies_est_alpha = med_est_alpha - alpha,
-        vies_est_beta = med_est_beta - beta
+    #### Calcular o viés relativo percentual
+    avaliacao_parcial_estimadores <- avaliacao_parcial_estimadores %>%
+        mutate(
+            vies_rel_est_alpha = (100 / alpha) * vies_est_alpha,
+            vies_rel_est_beta = (100 / beta) * vies_est_beta
+        )
+
+    #### Calcular a variância Monte Carlo
+    var_monte_carlo <- amostras_estimativas %>%
+        filter(n == t) %>%
+        mutate(
+            var_mc_est_alpha = var(est_alpha - mean(est_alpha)),
+            var_mc_est_beta = var(est_beta - mean(est_beta))
+        ) %>%
+        select(var_mc_est_alpha, var_mc_est_beta) %>%
+        slice(1)
+
+    avaliacao_parcial_estimadores <- bind_cols(
+        avaliacao_parcial_estimadores,
+        var_monte_carlo
     )
 
-#### Calcular o viés relativo percentual
-avaliacao_estimadores <- avaliacao_estimadores %>%
-    mutate(
-        vies_rel_est_alpha = (100 / alpha) * vies_est_alpha,
-        vies_rel_est_beta = (100 / beta) * vies_est_beta
+    #### Calcular o erro quadrático médio
+    erro_quad_medio <- amostras_estimativas %>%
+        filter(n == t) %>%
+        mutate(
+            eqm_est_alpha = var(est_alpha - alpha) * ((t) / (t - 1)),
+            eqm_est_beta = var(est_beta - beta) * ((t) / (t - 1))
+        ) %>%
+        select(eqm_est_alpha, eqm_est_beta) %>%
+        slice(1)
+
+    avaliacao_parcial_estimadores <- bind_cols(
+        avaliacao_parcial_estimadores,
+        erro_quad_medio
     )
 
-#### Calcular a variância Monte Carlo
-var_monte_carlo <- amostras %>%
-    filter(n == 200) %>%
-    mutate(
-        var_mc_est_alpha = var(est_alpha - mean(est_alpha)),
-        var_mc_est_beta = var(est_beta - mean(est_beta))
-    ) %>%
-    select(var_mc_est_alpha, var_mc_est_beta) %>%
-    slice(1)
+    if (t == 20) {
+        avaliacao_estimadores <- avaliacao_parcial_estimadores
+    } else {
+        avaliacao_estimadores <- bind_rows(
+            avaliacao_estimadores,
+            avaliacao_parcial_estimadores
+        )
+    }
+}
 
-avaliacao_estimadores <- bind_cols(avaliacao_estimadores, var_monte_carlo)
-
-#### Exibir a avaliação dos estimadores
+### Exibir a tabela com a avaliação dos estimadores
 avaliacao_estimadores
